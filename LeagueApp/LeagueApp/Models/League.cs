882771ba4a4e5b4a3adfd8680412d64234d253dc
wilk98 <maciekwilk09@gmail.com>
2024-02-16 16:53:47 +0100

    create League class in Models folder

---
+++ LeagueApp/LeagueApp/Models/League.cs
@@ -0,0 +1,15 @@
+﻿namespace LeagueApp.Models;
+
+public class League
+{
+    public int LeagueId { get; set; }
+    public string Name { get; set; }
+    public ICollection<Team> Teams { get; set; }
+    public ICollection<Match> Matches { get; set; }
+
+    public League()
+    {
+        Teams = new HashSet<Team>();
+        Matches = new HashSet<Match>();
+    }
+}
