679dd380e30705dd7a780071e2636ce9bdbc8086
wilk98 <maciekwilk09@gmail.com>
2024-02-16 16:52:02 +0100

    create Match class in Models folder

---
+++ LeagueApp/LeagueApp/Models/Match.cs
@@ -0,0 +1,15 @@
+﻿namespace LeagueApp.Models;
+
+public class Match
+{
+    public int MatchId { get; set; }
+    public int HomeTeamId { get; set; }
+    public Team HomeTeam { get; set; }
+    public int AwayTeamId { get; set; }
+    public Team AwayTeam { get; set; }
+    public int LeagueId { get; set; }
+    public League League { get; set; }
+    public DateTime MatchTime { get; set; }
+    public string Score { get; set; }
+    public bool IsBest { get; set; }
+}
