c78966cd2f16ada10ffff022665346ac449d0882
wilk98 <maciekwilk09@gmail.com>
2024-02-16 16:47:48 +0100

    create Team class in Models folder

---
+++ LeagueApp/LeagueApp/Models/Team.cs
@@ -0,0 +1,22 @@
+﻿namespace LeagueApp.Models;
+
+public class Team
+{
+    public int TeamId { get; set; }
+    public string Name { get; set; }
+    public int LeagueId { get; set; }
+    public League League { get; set; }
+    public TeamStats Stats { get; set; }
+
+
+    public ICollection<Match> HomeMatches { get; set; }
+    public ICollection<Match> AwayMatches { get; set; }
+    public ICollection<User> Fans { get; set; }
+
+    public Team()
+    {
+        HomeMatches = new HashSet<Match>();
+        AwayMatches = new HashSet<Match>();
+        Fans = new HashSet<User>();
+    }
+}
