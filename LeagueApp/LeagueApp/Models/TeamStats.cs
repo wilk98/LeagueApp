48b1792a301ff08e293a1641759adf90e5178c88
wilk98 <maciekwilk09@gmail.com>
2024-02-16 16:49:09 +0100

    create TeamStats class in Models folder

---
+++ LeagueApp/LeagueApp/Models/TeamStats.cs
@@ -0,0 +1,16 @@
+﻿namespace LeagueApp.Models;
+
+public class TeamStats
+{
+    public int TeamStatsId { get; set; }
+    public int TeamId { get; set; }
+    public Team Team { get; set; }
+    public int Points { get; set; }
+    public int MatchesPlayed { get; set; }
+    public int Wins { get; set; }
+    public int Draws { get; set; }
+    public int Losses { get; set; }
+    public int GoalsFor { get; set; }
+    public int GoalsAgainst { get; set; }
+    public int GoalDifference => GoalsFor - GoalsAgainst;
+}
