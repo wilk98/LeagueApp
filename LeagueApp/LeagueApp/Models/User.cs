85845265c9d563788246fb3e3d039e7ed83645fc
wilk98 <maciekwilk09@gmail.com>
2024-02-16 16:45:55 +0100

    create User in Models folder

---
+++ LeagueApp/LeagueApp/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeagueApp.Models;
+
+public class User : IdentityUser
+{
+    public User()
+    {
+        FavoriteTeams = new HashSet<Team>();
+    }
+
+    public virtual ICollection<Team> FavoriteTeams { get; set; }
+}
