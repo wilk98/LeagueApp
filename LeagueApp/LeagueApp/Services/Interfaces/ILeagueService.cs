6e0800c88db7a8fcb85cd032a0c83610f330de2e
wilk98 <maciekwilk09@gmail.com>
2024-02-18 22:43:35 +0100

    create interface of LeagueService

---
+++ LeagueApp/LeagueApp/Services/Interfaces/ILeagueService.cs
@@ -0,0 +1,12 @@
+﻿using LeagueApp.Models;
+
+namespace LeagueApp.Services;
+
+public interface ILeagueService
+{
+    Task<IEnumerable<League>> GetAllLeaguesAsync();
+    Task<League> GetLeagueByIdAsync(int id);
+    Task<League> CreateLeagueAsync(League league);
+    Task UpdateLeagueAsync(League league);
+    Task DeleteLeagueAsync(int id);
+}
