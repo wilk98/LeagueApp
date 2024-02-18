f6dd5cb592c301ea27b11e257baa4df547cb738e
wilk98 <maciekwilk09@gmail.com>
2024-02-18 22:46:55 +0100

    create class TeamStatsService

---
+++ LeagueApp/LeagueApp/Services/TeamStatsService.cs
@@ -0,0 +1,26 @@
+﻿using LeagueApp.Data;
+using LeagueApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeagueApp.Services;
+
+public class TeamStatsService : ITeamStatsService
+{
+    private readonly ApplicationDbContext _context;
+
+    public TeamStatsService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<TeamStats> GetTeamStatsByTeamIdAsync(int teamId)
+    {
+        return await _context.TeamStats.FirstOrDefaultAsync(ts => ts.TeamId == teamId);
+    }
+
+    public async Task UpdateTeamStatsAsync(TeamStats teamStats)
+    {
+        _context.Entry(teamStats).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+}
