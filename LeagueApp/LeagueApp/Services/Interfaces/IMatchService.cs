12fcdb5d4c4e8bf3a0178ce6b51209c634e3a80f
wilk98 <maciekwilk09@gmail.com>
2024-02-18 22:43:57 +0100

    create interface of MatchService

---
+++ LeagueApp/LeagueApp/Services/Interfaces/IMatchService.cs
@@ -0,0 +1,12 @@
+﻿using LeagueApp.Models;
+
+namespace LeagueApp.Services;
+
+public interface IMatchService
+{
+    Task<IEnumerable<Match>> GetAllMatchesAsync();
+    Task<Match> GetMatchByIdAsync(int id);
+    Task<Match> CreateMatchAsync(Match match);
+    Task UpdateMatchAsync(Match match);
+    Task DeleteMatchAsync(int id);
+}
