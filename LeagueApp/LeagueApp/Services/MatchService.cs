4c4ca70f3d557dbe8a267fd832e3495968122794
wilk98 <maciekwilk09@gmail.com>
2024-02-18 22:46:35 +0100

    create class MatchService with CRUD methods

---
+++ LeagueApp/LeagueApp/Services/MatchService.cs
@@ -0,0 +1,51 @@
+﻿using LeagueApp.Data;
+using LeagueApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeagueApp.Services;
+
+public class MatchService : IMatchService
+{
+    private readonly ApplicationDbContext _context;
+
+    public MatchService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Match>> GetAllMatchesAsync()
+    {
+        return await _context.Matches.Include(m => m.HomeTeam).Include(m => m.AwayTeam).ToListAsync();
+    }
+
+    public async Task<Match> GetMatchByIdAsync(int id)
+    {
+        return await _context.Matches
+            .Include(m => m.HomeTeam)
+            .Include(m => m.AwayTeam)
+            .FirstOrDefaultAsync(m => m.MatchId == id);
+    }
+
+    public async Task<Match> CreateMatchAsync(Match match)
+    {
+        _context.Matches.Add(match);
+        await _context.SaveChangesAsync();
+        return match;
+    }
+
+    public async Task UpdateMatchAsync(Match match)
+    {
+        _context.Entry(match).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteMatchAsync(int id)
+    {
+        var match = await _context.Matches.FindAsync(id);
+        if (match != null)
+        {
+            _context.Matches.Remove(match);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
