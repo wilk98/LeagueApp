6e08bc4a9af82dd4f5cfbdddeaa0476bced332b9
wilk98 <maciekwilk09@gmail.com>
2024-02-18 22:45:56 +0100

    create class TeamService with CRUD methods

---
+++ LeagueApp/LeagueApp/Services/TeamService.cs
@@ -0,0 +1,48 @@
+﻿using LeagueApp.Data;
+using LeagueApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeagueApp.Services;
+
+public class TeamService : ITeamService
+{
+    private readonly ApplicationDbContext _context;
+
+    public TeamService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Team>> GetAllTeamsAsync()
+    {
+        return await _context.Teams.ToListAsync();
+    }
+
+    public async Task<Team> GetTeamByIdAsync(int id)
+    {
+        return await _context.Teams.FindAsync(id);
+    }
+
+    public async Task<Team> CreateTeamAsync(Team team)
+    {
+        _context.Teams.Add(team);
+        await _context.SaveChangesAsync();
+        return team;
+    }
+
+    public async Task UpdateTeamAsync(Team team)
+    {
+        _context.Entry(team).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteTeamAsync(int id)
+    {
+        var team = await _context.Teams.FindAsync(id);
+        if (team != null)
+        {
+            _context.Teams.Remove(team);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
