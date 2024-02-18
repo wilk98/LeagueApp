86d98dc5cc7c691d5684125dbb0af4c6c1e63893
wilk98 <maciekwilk09@gmail.com>
2024-02-18 22:44:12 +0100

    create interface of TeamStatsService

---
+++ LeagueApp/LeagueApp/Services/Interfaces/ITeamStatsService.cs
@@ -0,0 +1,9 @@
+﻿using LeagueApp.Models;
+
+namespace LeagueApp.Services;
+
+public interface ITeamStatsService
+{
+    Task<TeamStats> GetTeamStatsByTeamIdAsync(int teamId);
+    Task UpdateTeamStatsAsync(TeamStats teamStats);
+}
