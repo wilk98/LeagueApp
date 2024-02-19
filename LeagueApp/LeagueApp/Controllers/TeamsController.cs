be9782ca168aa2a71155d1f0f9798db014bb9653
wilk98 <maciekwilk09@gmail.com>
2024-02-19 21:46:19 +0100

    create TeamsController

---
+++ LeagueApp/LeagueApp/Controllers/TeamsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LeagueApp.Services;
+using LeagueApp.Models;
+
+namespace LeagueApp.Controllers;
+
+public class TeamsController : Controller
+{
+    private readonly ITeamService _teamService;
+
+    public TeamsController(ITeamService teamService)
+    {
+        _teamService = teamService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var teams = await _teamService.GetAllTeamsAsync();
+        return View(teams);
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        var team = await _teamService.GetTeamByIdAsync(id);
+        if (team == null)
+        {
+            return NotFound();
+        }
+        return View(team);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([Bind("TeamId,Name")] Team team)
+    {
+        if (ModelState.IsValid)
+        {
+            await _teamService.CreateTeamAsync(team);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(team);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var team = await _teamService.GetTeamByIdAsync(id);
+        if (team == null)
+        {
+            return NotFound();
+        }
+        return View(team);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, [Bind("TeamId,Name")] Team team)
+    {
+        if (id != team.TeamId)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            await _teamService.UpdateTeamAsync(team);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(team);
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var team = await _teamService.GetTeamByIdAsync(id);
+        if (team == null)
+        {
+            return NotFound();
+        }
+        return View(team);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _teamService.DeleteTeamAsync(id);
+        return RedirectToAction(nameof(Index));
+    }
+
+
+}
