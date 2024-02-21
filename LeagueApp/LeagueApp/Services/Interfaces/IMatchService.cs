d9fce3a460c90889f6fc4d9e0e86f5d007948846
wilk98 <maciekwilk09@gmail.com>
2024-02-21 20:16:37 +0100

    add GetMatchesByTeamIdAsync to interface of MatchService

--- LeagueApp/LeagueApp/Services/Interfaces/IMatchService.cs
+++ LeagueApp/LeagueApp/Services/Interfaces/IMatchService.cs
@@ -5,6 +5,7 @@ namespace LeagueApp.Services;
 public interface IMatchService
 {
     Task<IEnumerable<Match>> GetAllMatchesAsync(int leagueId);
+    Task<IEnumerable<Match>> GetMatchesByTeamIdAsync(int teamId);
     Task<Match> GetMatchByIdAsync(int id);
     Task<Match> CreateMatchAsync(Match match);
     Task UpdateMatchAsync(Match match);
