dfc88930f74ed7afc21d2380dcbd70e17d39a504
wilk98 <maciekwilk09@gmail.com>
2024-02-21 20:17:04 +0100

    create interface of UserService

---
+++ LeagueApp/LeagueApp/Services/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using LeagueApp.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace LeagueApp.Services.Interfaces;
+
+public interface IUserService
+{
+    Task<IdentityResult> RegisterUserAsync(RegisterViewModel model);
+    Task<SignInResult> LoginUserAsync(LoginViewModel model);
+    Task LogoutUserAsync();
+    Task<IdentityResult> AddTeamToFavoritesAsync(string userId, int teamId);
+    Task<IdentityResult> RemoveTeamFromFavoritesAsync(string userId, int teamId);
+    Task<List<int>> GetUserFavoriteTeamIdsAsync(string userId);
+}
+
