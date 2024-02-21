2f7da65721d714ea93a65e9d2d2ade6f9ca63b9c
wilk98 <maciekwilk09@gmail.com>
2024-02-21 20:29:16 +0100

    Update TeamsController (Index)

--- LeagueApp/LeagueApp/Controllers/TeamsController.cs
+++ LeagueApp/LeagueApp/Controllers/TeamsController.cs
@@ -1,21 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using LeagueApp.Services;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using LeagueApp.Models;
+using LeagueApp.Services;
+using LeagueApp.Services.Interfaces;
 
 namespace LeagueApp.Controllers;
 
 public class TeamsController : Controller
 {
     private readonly ITeamService _teamService;
+    private readonly UserManager<User> _userManager;
+    private readonly ILogger<TeamsController> _logger;
+    private readonly IUserService _userService;
 
-    public TeamsController(ITeamService teamService)
+    public TeamsController(ITeamService teamService, UserManager<User> userManager, ILogger<TeamsController> logger, IUserService userService)
     {
         _teamService = teamService;
+        _userManager = userManager;
+        _logger = logger;
+        _userService = userService;
     }
 
     public async Task<IActionResult> Index(int leagueId)
     {
+        _logger.LogInformation("Wywołanie metody Index dla ligi o ID: {LeagueId}", leagueId);
         var teams = await _teamService.GetAllTeamsAsync(leagueId);
+        _logger.LogInformation("Pobrano {Count} drużyn dla ligi o ID: {LeagueId}", teams.Count(), leagueId);
+
+        var user = await _userManager.GetUserAsync(User);
+        if (user != null)
+        {
+            _logger.LogInformation("Zalogowany użytkownik: {UserName}", user.UserName);
+            var favoriteTeamIds = await _userService.GetUserFavoriteTeamIdsAsync(user.Id);
+            _logger.LogInformation("Znaleziono {Count} ulubionych drużyn dla użytkownika {UserName}", favoriteTeamIds.Count, user.UserName);
+            ViewBag.FavoriteTeamIds = favoriteTeamIds;
+        }
+        else
+        {
+            _logger.LogWarning("Nie znaleziono zalogowanego użytkownika");
+            ViewBag.FavoriteTeamIds = new List<int>();
+        }
         return View(teams);
     }
 
@@ -82,6 +106,4 @@ public class TeamsController : Controller
         await _teamService.DeleteTeamAsync(id);
         return RedirectToAction(nameof(Index));
     }
-
-
 }
